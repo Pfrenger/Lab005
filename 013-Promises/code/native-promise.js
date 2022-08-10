7bcbe078ab00ad491cfc42fedf288cf53cb7c6c6
carpaltunnel <43389053+carpaltunnel@users.noreply.github.com>
2022-08-10 17:36:20 -0500

    promises

---
+++ 013-Promises/code/native-promise.js
@@ -0,0 +1,21 @@
+const fiveSecondPromise = (seconds) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+fiveSecondPromise(5)
+.then(() => {
+  console.log('Looks like it\'s been five seconds...');
+}).catch((err) => {
+  console.error(`An error/reject occurred!  The error was : ${err}`);
+}).finally(() => {
+  console.log('Everything in the promise chain is done executing');
+});
+
+console.log('I wonder when the promise will resolve...');
+
+
+const x = 5;
+const y = x;
+const q = 'this is at est';
