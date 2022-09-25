5392ac4a029c4881a4781394a7dcfd58c4d00792
Austin <austinpfrenger@gmail.com>
2022-09-25 10:33:13 -0500

    first commit

---
+++ lab001.js
@@ -0,0 +1,13 @@
+let helloWorld = 'Hello, World!' ;
+let daysOfTheYear = 365 ; 
+const varIsEvil = true ;
+let allWorkAndNoPlayMakesMeANullBoy = null ; 
+console.log(typeof helloWorld);
+console.log(typeof daysOfTheYear) ;
+console.log(typeof varIsEvil) ;
+console.log(typeof allWorkAndNoPlayMakesMeANullBoy)
+console.log(helloWorld) ;
+console.log(daysOfTheYear) ;
+console.log(varIsEvil) ;
+console.log(allWorkAndNoPlayMakesMeANullBoy) ;
+// It turns out I just needed to turn it in to be able to see what I did wrong
---
+++ saturdaynotes.js
@@ -0,0 +1,16 @@
+const randomString = 'my computer\'s name is \n \t "bear"'
+//backslash indicates to the interpreter the character is not a part of the sytax
+//${} for template literals 
+const randomString1 = '10 \\ 2'
+//second backslash escapes the second backslash resulting in "10\2"
+console.log(randomString);
+console.log('\t\t and it is purple' )
+//console.log(randomString);
+
+//console.log(randomString);
+// \ escape character
+// \n new line character 
+// \t tab character 
+// \& uses ampersand 
+// \b backspace 
+// \f form feed 
